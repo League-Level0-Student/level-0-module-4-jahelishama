8fbd76e386155cea2d92ca5d21eff679d712a561
ilabstaff <ilabstaff@172.16.116.68>
2019-05-28 19:16:55 -0700

    bye

---
+++ src/extra/SimpleSorter.java
@@ -0,0 +1,13 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class SimpleSorter {
+	public static void main(String[] args) {
+	String me=JOptionPane.showInputDialog("Give me a number and reapet it three times");
+	}
+}
+
+
+
+
